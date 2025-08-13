4b7e942867b5cfe41c8b541eb4280189950b0b8f
Arthur Mamedes Borges <arthurmamedesborges@gmail.com>
2025-08-13 20:20:42 +0000

    Ajustes

--- master.py
+++ master.py
@@ -10,7 +10,7 @@ MIN_DATE = datetime(2024, 1, 1)
 MAX_DATE = datetime(2050, 12, 31)
 DATA_CONCLUSAO = 7306
 
-st.set_page_config(page_title="ggdp3", layout="wide")
+st.set_page_config(page_title="GGDP", layout="wide")
 st.markdown(
         """
         <style>
