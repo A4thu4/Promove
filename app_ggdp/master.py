30c826599013c9d839aab967380866ccb03d6d43
Arthur Mamedes Borges <arthurmamedesborges@gmail.com>
2025-10-07 20:22:08 +0000

    Revert "ajuste"
    This reverts commit 333a260ebdf9760ca52912d46c4a6fc23721c858.

--- app_ggdp/master.py
+++ app_ggdp/master.py
@@ -56,6 +56,7 @@ st.markdown(
     </style>
     """,unsafe_allow_html=True)
 
+
 #BOTÕES
 st.markdown(
     """
@@ -121,6 +122,7 @@ st.markdown(
     </style>
     """, unsafe_allow_html=True
 ) 
+
 
 def main():
     # ---------- NAVEGAÇÃO ---------- #
