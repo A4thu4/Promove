30813024522835af06ce6abe9f04480fb8d4cdab
Arthur Mamedes Borges <arthurmamedesborges@gmail.com>
2025-10-09 19:09:52 +0000

    adicionando imagens

--- app_ggdp/master.py
+++ app_ggdp/master.py
@@ -7,7 +7,10 @@ from layout import ensure_states, build_obrigatorios, build_afastamentos, build_
 ensure_states()
 from data_utils import DATA_CONCLUSAO
 
-st.set_page_config(page_title="SIMULADOR GGDP", layout="wide")
+st.set_page_config(page_title="SIMULADOR GGDP", page_icon="assets/Brasão.png", layout="wide")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("assets/Logomarca SEAD.png", width=800)
 
 st.markdown(
     """
