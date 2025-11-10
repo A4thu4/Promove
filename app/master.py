68e26d5f3576f1c2bca213d3e8f1a6bffd85ad24
Arthur Mamedes Borges <arthurmamedesborges@gmail.com>
2025-11-10 18:22:24 +0000

    Revert "ajustando aparencia v4"
    This reverts commit b7b96792d5995eaab6267504186bc81a3db1e922.

--- app/master.py
+++ app/master.py
@@ -333,7 +333,7 @@ def main():
         
         col1, col2 = st.columns([0.05, 1.93])
         with col1: 
-            st.markdown(bloco_vertical("", 2140, "#fede01"), unsafe_allow_html=True)
+            st.markdown(bloco_vertical("", 2160, "#fede01"), unsafe_allow_html=True)
         with col2:
             build_titulacoes()
             st.divider()
