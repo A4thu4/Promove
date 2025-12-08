9ea9ded1525960688c5c62cdaba066ceb86c77d9
Arthur Mamedes Borges <arthurmamedesborges@gmail.com>
2025-12-08 10:12:20 -0300

    Atualizar o logic_ueg.py

--- ueg/logic_ueg.py
+++ ueg/logic_ueg.py
@@ -592,7 +592,7 @@ def calcular_planilha(arquivo):
         lambda x: f"{x:>5}" if isinstance(x, int) or (isinstance(x, str) and x.isdigit()) else x
     )
 
-    st.dataframe(df_results.head(1).style.map(destacar_obs, subset=["Observação"]), hide_index=True)
+    st.dataframe(df_results.style.map(destacar_obs, subset=["Observação"]), hide_index=True)
 
     if len(ids_processados) == 1:
         st.markdown("<h3 style='text-align:center;'>Pontuações Mensais</h3>", unsafe_allow_html=True)
